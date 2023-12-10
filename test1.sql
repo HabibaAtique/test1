f05c1767efdb9c204c1ad28d71654e69eaef6e11
HabibaAtique <56055664+HabibaAtique@users.noreply.github.com>
2023-12-10 22:46:33 +0500

    Add files via upload

---
+++ test1.sql
@@ -0,0 +1,21 @@
+-- Drop the TravelPortal table
+DROP TABLE TravelPortal;
+
+-- Drop the Ticket table
+DROP TABLE Ticket;
+
+-- Drop the Service table
+DROP TABLE Service;
+
+-- Drop the Stop table
+DROP TABLE Stop;
+
+-- Drop the Agent table
+DROP TABLE Agent;
+
+-- Drop the Route table
+DROP TABLE Route;
+
+-- Drop the Route table
+drop table Client;
+
